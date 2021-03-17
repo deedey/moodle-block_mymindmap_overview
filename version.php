76cf976a05c35bee9abecbcb91e16dcede4d6133
Dey Bendifallah <d.bendifallah@yahoo.com>
2021-03-17 08:53:10 +0100

    Update version.php

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->release = '1.3.1 (Build: 2021010700)';
-$plugin->version   = 2021010700;
+$plugin->release = '1.3.2 (Build: 2021031500)';
+$plugin->version   = 2021031500;
 $plugin->requires  = 2015102300;
 $plugin->component = 'block_mymindmap_overview';
 $plugin->maturity = MATURITY_STABLE;
